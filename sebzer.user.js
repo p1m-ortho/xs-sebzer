391c13fd12fae7eb56ee700134e1eeb56008f33f
Павел Желнов <pzhelnov@p1m.org>
2019-05-12 16:02:59 +0300

    Allow cyrillic chars in the article number
    An example below:
    
    https://elibrary.ru/item.asp?id=28342227
    
    ХИРУРГИЧЕСКОЕ ЛЕЧЕНИЕ ПОСТРАДАВШИХ С ДИСТРАКЦИОННЫМИ И ТРАНСЛЯЦИОННЫМИ ПОВРЕЖДЕНИЯМИ ПОЗВОНОЧНИКА ГРУДНОЙ И ПОЯСНИЧНОЙ ЛОКАЛИЗАЦИИ
    Дулаев А.К., Аликов З.Ю., Горанчук Д.В., Дулаева Н.М., Абуков Д.Н., Мушкин М.А.
    Кафедра травматологии и ортопедии. 2016. № Спецвыпуск. С. 95.
    
    Found while doing xt-ao-type-a (see current commits).

--- sebzer.user.js
+++ sebzer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         sebzer
 // @namespace    http://p1m.org/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Средство экспорта библиографических записей из eLIBRARY.RU (СЕБЗЕР). Добавляет в eLIBRARY.RU возможности экспорта библиографических записей, подобные таковым в PubMed. В настоящее время поддерживается экспорт только со страниц выдачи, только с ограничением по типу публикации «статьи в журналах» и только в формате BibTeX.
 // @author       Павел Желнов
 // @match        http*://elibrary.ru/*
@@ -32,7 +32,7 @@
     var mime = "application/x-bibtex;charset=utf-8";
     var filename = 'elibrary_ru';
     var ext = "bib";
-    var ref_regex = /^(.+)\. (\d{4})\. (Т\.? ([\d\w\-\(\) ]+)\. )?(№\.? ([\d\w\-\(\) ]+)\. )?(С)\. ([\d\w\-]+)\.$/i;
+    var ref_regex = /^(.+)\. (\d{4})\. (Т\.? ([\d\w\-\(\) ]+)\. )?(№\.? ([\d\wА-я\-\(\) ]+)\. )?(С)\. ([\d\w\-]+)\.$/i;
     var au_regex = /^(.*?) ?([^ ]+?)\.?$/i;
     var bib_regex = /^@\w+{.*,$/gmi;
 
