af9280add01fa4625de44e21a06d5e050093b85c
Павел Желнов <pzhelnov@p1m.org>
2019-11-02 00:10:00 +0300

    Resolve nested $(document).ready() calls
    Doesn’t seem to change a thing, but the nesting
    still was unnecessary.

--- sebzer.user.js
+++ sebzer.user.js
@@ -11,7 +11,7 @@
 // @require      https://cdn.rawgit.com/larsgw/citation.js/archive/citation.js/citation-0.4.0-10.min.js
 // ==/UserScript==
 
-(function() {
+$(function() {
     'use strict';
     var elibrary = "https://elibrary.ru";
     const Cite = require('citation-js');
@@ -38,105 +38,103 @@
     var au_regex = /^(.*?) ?([^ ]+?)\.?$/i;
     var bib_regex = /^@\w+{.*,$/gmi;
 
-    $(document).ready(function() {
-        $('#restab').before(canvas);
-        $('#sebzer-canvas').attr("style", style);
-        if ($('#thepage').prop('outerHTML').toLowerCase().indexOf('всего найдено публикаций:') === -1)
-            e.notSearch = true;
-        else {
-            $('tr').filter(function(){
-                return this.id.match(/^a\d+$/g);
-            })
-            .children().filter('td[align="left"]').each(function(){
-                var bibjson = {type: "misc"};
-                var bibkey = $(this).parent().attr('id');;
-                var hasAuthor = ($('font',this).has('i').length == 1) ? true : false;
-                if (hasAuthor) {
-                    var author = [];
-                    var austr = $(this).children().has('i').not(':has(b)').text().replace('et al.','').trim();
-                    var auarr = austr.split(', ');
-                    auarr.forEach(function(val, i) {
-                        var au = val.match(au_regex);
-                        var name;
-                        if (au[1]) {
-                            var lastname = au[1];
-                            var firstname = au[2].replace(/[^a-za-я\d]+/gi,'. ')+".";
-                            name = lastname + ', ' + firstname;
-                            author.push({
-                                lastname: lastname,
-                                firstname: firstname,
-                                name: name
-                            });
-                        }
-                        else {
-                            name = au[2];
-                            author.push({
-                                name: name
-                            });
-                        }
-                    });
-                    bibjson.author = JSON.parse(JSON.stringify(author));
-                }
-                var hasTitle = ($(this).has('b').length) ? true : false;
-                if (hasTitle)
-                    bibjson.title = $(this).children().has('b').text().trim();
-                var hasRef = ($(this).children().not(':has(i),:has(b)').length) ? true : false;
-                if (hasRef) {
-                    var refstr = $(this).children().not(':has(i),:has(b)').text().replace(/\s+/g,' ').trim();
-                    var refarr = refstr.match(ref_regex);
-                    if (refarr) {
-                        bibjson.type = "article";
-                        bibjson.journal = {name: refarr[1]};
-                        bibjson.year = refarr[2];
-                        if (refarr[4])
-                            bibjson.journal.volume = refarr[4].replace(/^(\d+).*$/,"$1");
-                        if (refarr[6])
-                            bibjson.journal.issue = refarr[6].replace(/^(\d+).*$/,"$1");
-                        bibjson.journal.pages = refarr[8].replace('-','--');
-                        if (!hasAuthor)
-                            bibjson.author = [{name: "Б.а."}];
-                        if (!hasTitle)
-                            bibjson.title = "Б.н.";
+    $('#restab').before(canvas);
+    $('#sebzer-canvas').attr("style", style);
+    if ($('#thepage').prop('outerHTML').toLowerCase().indexOf('всего найдено публикаций:') === -1)
+        e.notSearch = true;
+    else {
+        $('tr').filter(function(){
+            return this.id.match(/^a\d+$/g);
+        })
+        .children().filter('td[align="left"]').each(function(){
+            var bibjson = {type: "misc"};
+            var bibkey = $(this).parent().attr('id');;
+            var hasAuthor = ($('font',this).has('i').length == 1) ? true : false;
+            if (hasAuthor) {
+                var author = [];
+                var austr = $(this).children().has('i').not(':has(b)').text().replace('et al.','').trim();
+                var auarr = austr.split(', ');
+                auarr.forEach(function(val, i) {
+                    var au = val.match(au_regex);
+                    var name;
+                    if (au[1]) {
+                        var lastname = au[1];
+                        var firstname = au[2].replace(/[^a-za-я\d]+/gi,'. ')+".";
+                        name = lastname + ', ' + firstname;
+                        author.push({
+                            lastname: lastname,
+                            firstname: firstname,
+                            name: name
+                        });
                     }
                     else {
-                        e.notArticle = true;
-                        bibjson.note = refstr;
-                        if (refstr.match(/^\d{4}$/))
-                            bibjson.year = refstr;
+                        name = au[2];
+                        author.push({
+                            name: name
+                        });
                     }
-                }
-                var hasUrl = ($(this).children().eq(0).has('[href]')) ? true : false;
-                if (hasUrl) {
-                    var url = elibrary + $(this).children().eq(0).attr('href');
-                    var link = [{url: url}];
-                    bibjson.link = link;
-                }
-                var bibtex = new Cite(bibjson).format('bibtex');
-                var label = "@" + bibjson.type + "{" + bibkey + ",";
-                bibtex = bibtex.replace(bib_regex, label);
-                record.add(bibtex);
-            });
-        }
-
-        for (var note in e) {
-            if (e[note]) {
-                $('#sebzer-canvas').append(eSebzerNote);
-                break;
+                });
+                bibjson.author = JSON.parse(JSON.stringify(author));
             }
-        }
+            var hasTitle = ($(this).has('b').length) ? true : false;
+            if (hasTitle)
+                bibjson.title = $(this).children().has('b').text().trim();
+            var hasRef = ($(this).children().not(':has(i),:has(b)').length) ? true : false;
+            if (hasRef) {
+                var refstr = $(this).children().not(':has(i),:has(b)').text().replace(/\s+/g,' ').trim();
+                var refarr = refstr.match(ref_regex);
+                if (refarr) {
+                    bibjson.type = "article";
+                    bibjson.journal = {name: refarr[1]};
+                    bibjson.year = refarr[2];
+                    if (refarr[4])
+                        bibjson.journal.volume = refarr[4].replace(/^(\d+).*$/,"$1");
+                    if (refarr[6])
+                        bibjson.journal.issue = refarr[6].replace(/^(\d+).*$/,"$1");
+                    bibjson.journal.pages = refarr[8].replace('-','--');
+                    if (!hasAuthor)
+                        bibjson.author = [{name: "Б.а."}];
+                    if (!hasTitle)
+                        bibjson.title = "Б.н.";
+                }
+                else {
+                    e.notArticle = true;
+                    bibjson.note = refstr;
+                    if (refstr.match(/^\d{4}$/))
+                        bibjson.year = refstr;
+                }
+            }
+            var hasUrl = ($(this).children().eq(0).has('[href]')) ? true : false;
+            if (hasUrl) {
+                var url = elibrary + $(this).children().eq(0).attr('href');
+                var link = [{url: url}];
+                bibjson.link = link;
+            }
+            var bibtex = new Cite(bibjson).format('bibtex');
+            var label = "@" + bibjson.type + "{" + bibkey + ",";
+            bibtex = bibtex.replace(bib_regex, label);
+            record.add(bibtex);
+        });
+    }
 
-        if(e.notSearch)
-            $('#sebzer-canvas').append(eNotSearchNote);
-        else {
-            if(e.notArticle)
-                $('#sebzer-canvas').append(eNotArticleNote);
-
-            $('#sebzer-canvas').append(button);
-            $('#sebzer-button').click(function () {
-                var text = record.format('bibtex');
-                var blob = new Blob([text], {type: mime});
-                saveAs(blob, filename+"."+ext);
-            });
+    for (var note in e) {
+        if (e[note]) {
+            $('#sebzer-canvas').append(eSebzerNote);
+            break;
         }
-    });
-})();
+    }
+
+    if(e.notSearch)
+        $('#sebzer-canvas').append(eNotSearchNote);
+    else {
+        if(e.notArticle)
+            $('#sebzer-canvas').append(eNotArticleNote);
+
+        $('#sebzer-canvas').append(button);
+        $('#sebzer-button').click(function () {
+            var text = record.format('bibtex');
+            var blob = new Blob([text], {type: mime});
+            saveAs(blob, filename+"."+ext);
+        });
+    }
+});
